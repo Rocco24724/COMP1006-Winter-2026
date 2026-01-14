b487d4c6f2b53e16c8773a438f25e5cb8fbe4a51
Rocco <roccominetola24@gmail.com>
2026-01-14 15:45:26 -0500

    Week #2 Lesson

--- Week 2/index.php
+++ Week 2/index.php
@@ -1,14 +1,38 @@
-//1. Set Up & Start 
+<?php
+declare(strict_types=1);
 
+//inline Comment
 
-//2. Code Commenting 
-
+/*
+multi-line Comment
+*/
 
 //3. Variables, Data Tyoes, Concatenation, Conditional Statements & Echo
 
+$firstName = "Rocco";
+$lastName = "Minetola";
+$age = 20;
+$bool = true;
+
+echo "<p> Hello, my name is " . $firstName . " " . $lastName . "</p>";
+
+if ($bool) {
+    echo "<p> I am True </p>";
+}
+else {
+    echo "<p> I am False </p>";
+}
 
 //4. Loosely Typed Language Demo
 
+$num1 = 1;
+$num2 = 10;
+
+function add(int $num1, int $num2) : int {
+    return $num1 + $num2;
+}
+
+echo "<p>" . add($num1, $num2) . "</p>";
 
 //5. Strict Types & Types Hints
 
